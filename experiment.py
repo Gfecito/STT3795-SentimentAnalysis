17a340f835e7450394cd6ffdb7d1fba94bb1581d
Jeremy Qin <jeremyqin1@gmail.com>
2023-04-21 22:28:30 -0400

    Update for ensemble method

--- experiment.py
+++ experiment.py
@@ -6,7 +6,7 @@ default_params = {
     "optimizer": "Adam",
     "lr": 0.001,
     "loss": "binary_crossentropy",
-    "batch_size": 16,
+    "batch_size": 1024,
     "epochs": 10,
     "model": "RNN",
 }
@@ -68,6 +68,27 @@ def stem(x_train):
 
     return x_train
 
+def plot_history(history):
+    import matplotlib.pyplot as plt
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    x = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(x, acc, 'b', label='Train accuracy')
+    plt.plot(x, val_acc, 'r', label='Val accuracy')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, 'b', label='Train loss')
+    plt.plot(x, val_loss, 'r', label='Val loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+
 def experiment(params):
     import tensorflow as tf
     from tensorflow import keras
@@ -80,6 +101,10 @@ def experiment(params):
     from sklearn.model_selection import train_test_split
     from sklearn.preprocessing import LabelEncoder
     from sklearn.linear_model import LogisticRegression
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    import matplotlib.pyplot as plt
+    from sklearn.metrics import classification_report
+    plt.style.use('ggplot')
 
     root_data = params["root_data"]
     maxlength = params["maxlength"]
@@ -110,67 +135,207 @@ def experiment(params):
 
     X_train, X_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.02, random_state=22)
 
-    #Tokenizer
-    tokenizer = keras.preprocessing.text.Tokenizer()
-    tokenizer.fit_on_texts(X_train)
-    vocab_size = len(tokenizer.word_index) + 1
-    texts_to_int = tokenizer.texts_to_sequences(X_train)
-    #Padding
-    texts_to_int_pad = keras.preprocessing.sequence.pad_sequences(texts_to_int,
-                                                                maxlen=maxlength,)
-    texts_to_int_test = tokenizer.texts_to_sequences(X_test)
-    texts_to_int_pad_test = keras.preprocessing.sequence.pad_sequences(texts_to_int_test,
-                                                                maxlen=maxlength,)
-    
-    X_train = texts_to_int_pad
-    X_test = texts_to_int_pad_test
-
-    #label encoding
-    labels = ["Negative", "Positive"]
-    encoder = LabelEncoder()
-    encoder.fit(y_train)
-    y_train = encoder.transform(y_train)
-    y_test = encoder.transform(y_test)
-    y_train = y_train.reshape(-1, 1)
-    y_test = y_test.reshape(-1, 1)
-
-
     if model_type == "RNN":
-    #Model 1
+        #Tokenizer
+        tokenizer = keras.preprocessing.text.Tokenizer()
+        tokenizer.fit_on_texts(X_train)
+        vocab_size = len(tokenizer.word_index) + 1
+        texts_to_int = tokenizer.texts_to_sequences(X_train)
+        #Padding
+        texts_to_int_pad = keras.preprocessing.sequence.pad_sequences(texts_to_int,
+                                                                    maxlen=maxlength,)
+        texts_to_int_test = tokenizer.texts_to_sequences(X_test)
+        texts_to_int_pad_test = keras.preprocessing.sequence.pad_sequences(texts_to_int_test,
+                                                                    maxlen=maxlength,)
+        
+        X_train = texts_to_int_pad
+        X_test = texts_to_int_pad_test
+
+        #label encoding
+        labels = ["Negative", "Positive"]
+        encoder = LabelEncoder()
+        encoder.fit(y_train)
+        y_train = encoder.transform(y_train)
+        y_test = encoder.transform(y_test)
+        y_train = y_train.reshape(-1, 1)
+        y_test = y_test.reshape(-1, 1)
+
+        #Model 1
         emb_dim = emb_dim
 
         inputs = Input(shape=(maxlength, ))
 
-        embedding_layer = Embedding(vocab_size, emb_dim, input_length=maxlength)
-        x = embedding_layer(inputs)
-        x = SpatialDropout1D(0.2)(x)
-        x = Conv1D(32, 5, activation='relu')(x)
-        # Passed on to the LSTM layer
-        x = Bidirectional(LSTM(32, dropout=0.4, recurrent_dropout=0.4))(x)
-        x = Dense(128, activation='relu')(x)
-        x = Dropout(0.5)(x)
-        x = Dense(64, activation='relu')(x)
-        # Passed on to activation layer to get final output
-        out = Dense(1, activation='sigmoid')(x)
+        model1 = Sequential()
+        model1.add(Embedding(vocab_size, emb_dim, input_length=maxlength))
+        model1.add(SpatialDropout1D(0.2))
+        model1.add(Conv1D(32, 5, activation='relu'))
+        model1.add(Bidirectional(LSTM(32, dropout=0.4, recurrent_dropout=0.4)))
+        model1.add(Dense(128, input_dim=input_dim, activation='relu'))
+        model1.add(Dropout(0.5))
+        model1.add(Dense(64, activation='relu'))
+        model1.add(Dropout(0.5))
+        model1.add(Dense(2, activation='softmax'))
 
-        model = Model(inputs=inputs, outputs=out)
         opt = optimizer_function(optimizer_string, learning_rate)
-        model.compile(optimizer=opt, loss=loss, metrics=["accuracy"])
+        model1.compile(optimizer=opt, loss=loss, metrics=["accuracy"])
         #Train
-        history = model.fit(X_train, y_train, 
+        history = model1.fit(X_train, y_train, 
                         batch_size=batch_size, epochs=epochs, 
                         validation_data=(X_test, y_test),
                     )
-        score = model.evaluate(X_test, y_test, verbose=0)
+        plot_history(history=history)
+        score = model1.evaluate(X_test, y_test, verbose=1)
         print('Test loss:', score[0]) 
         print('Test accuracy:', score[1])
+        preds1 = model1.predict(X_test)
+        classes1 = np.argmax(preds1, axis=1)
+        print(classification_report(y_test, classes1))
 
     elif model_type == "LogisticRegression":
+        vectorizer = TfidfVectorizer()
+        X_train = vectorizer.fit_transform(X_train).astype('float16')
+        X_test = vectorizer.transform(X_test).astype('float16')
+
+        encoder = LabelEncoder()
+        encoder.fit(y_train)
+        y_train = encoder.transform(y_train)
+        y_test = encoder.transform(y_test)
+
         model = LogisticRegression()
         model.fit(X_train, y_train)
         model_score = model.score(X_test, y_test)
         print('Logistic Regression Accuracy:', model_score)
+
+        preds2 = model2.predict_proba(X_test)
+        classes2 = np.argmax(preds2, axis=1)
+        print(classification_report(y_test, classes2))
+
+    elif model_type == "MLP":
+        vectorizer = TfidfVectorizer()
+        X_train = vectorizer.fit_transform(X_train).astype('float16')
+        X_test = vectorizer.transform(X_test).astype('float16')
+
+        encoder = LabelEncoder()
+        encoder.fit(y_train)
+        y_train = encoder.transform(y_train)
+        y_test = encoder.transform(y_test)
+
+        input_dim = X_train.shape[1]  # Number of features
+
+        model3 = Sequential()
+        model3.add(Dense(64, input_dim=input_dim, activation='relu'))
+        model3.add(Dense(32, activation='relu'))
+        model3.add(Dense(2, activation='softmax'))
+        opt = optimizer_function(optimizer_string, learning_rate)
+        model3.compile(optimizer=opt, loss=loss, metrics=["accuracy"])
+        history = model3.fit(X_train, y_train, 
+                    batch_size=batch_size, epochs=2, 
+                   )
+        plot_history(history=history)
+        score = model3.evaluate(X_test, y_test, verbose=0)
+        print('Test loss:', score[0]) 
+        print('Test accuracy:', score[1])
+        preds3 = model3.predict(X_test)
+        classes3 = np.argmax(preds3, axis=1)
+        print(classification_report(y_test, classes3))
     
+    elif model_type == "Ensemble":
+        vectorizer = TfidfVectorizer()
+        X_train_vect = vectorizer.fit_transform(X_train).astype('float16')
+        X_test_vect = vectorizer.transform(X_test).astype('float16')
+
+        #Tokenizer
+        tokenizer = keras.preprocessing.text.Tokenizer()
+        tokenizer.fit_on_texts(X_train)
+        vocab_size = len(tokenizer.word_index) + 1
+        texts_to_int = tokenizer.texts_to_sequences(X_train)
+        #Padding
+        texts_to_int_pad = keras.preprocessing.sequence.pad_sequences(texts_to_int,
+                                                                    maxlen=maxlength,)
+        texts_to_int_test = tokenizer.texts_to_sequences(X_test)
+        texts_to_int_pad_test = keras.preprocessing.sequence.pad_sequences(texts_to_int_test,
+                                                                    maxlen=maxlength,)
+        
+        X_train = texts_to_int_pad
+        X_test = texts_to_int_pad_test
+
+        #label encoding
+        labels = ["Negative", "Positive"]
+        encoder = LabelEncoder()
+        encoder.fit(y_train)
+        y_train_vect = encoder.transform(y_train)
+        y_test_vect = encoder.transform(y_test)
+        y_train = y_train.reshape(-1, 1)
+        y_test = y_test.reshape(-1, 1)
+
+        #Model 1
+        emb_dim = emb_dim
+
+        inputs = Input(shape=(maxlength, ))
+
+        model1 = Sequential()
+        model1.add(Embedding(vocab_size, emb_dim, input_length=maxlength))
+        model1.add(SpatialDropout1D(0.2))
+        model1.add(Conv1D(32, 5, activation='relu'))
+        model1.add(Bidirectional(LSTM(32, dropout=0.4, recurrent_dropout=0.4)))
+        model1.add(Dense(128, input_dim=input_dim, activation='relu'))
+        model1.add(Dropout(0.5))
+        model1.add(Dense(64, activation='relu'))
+        model1.add(Dropout(0.5))
+        model1.add(Dense(2, activation='softmax'))
+
+        opt = optimizer_function(optimizer_string, learning_rate)
+        model1.compile(optimizer=opt, loss=loss, metrics=["accuracy"])
+        #Train
+        history = model1.fit(X_train, y_train, 
+                        batch_size=batch_size, epochs=epochs, 
+                        validation_data=(X_test, y_test),
+                    )
+        print("Model1")
+        plot_history(history=history)
+
+        #Model 2
+        model2 = LogisticRegression()
+        model2.fit(X_train_vect, y_train_vect)
+
+        #Model3
+        input_dim = X_train_vect.shape[1]  # Number of features
+
+        model3 = Sequential()
+        model3.add(Dense(64, input_dim=input_dim, activation='relu'))
+        model3.add(Dense(32, activation='relu'))
+        model3.add(Dense(2, activation='softmax'))
+        opt = optimizer_function(optimizer_string, learning_rate)
+        model3.compile(optimizer=opt, loss=loss, metrics=["accuracy"])
+        history3 = model3.fit(X_train_vect, y_train_vect, 
+                    batch_size=1024, epochs=2, 
+                   )
+        print("Model3")
+        plot_history(history=history3)
+
+        preds1 = model1.predict(X_test)
+        classes1 = np.argmax(preds1, axis=1)
+        preds2 = model2.predict_proba(X_test_vect)
+        classes2 = np.argmax(preds2, axis=1)
+        preds3 = model3.predict(X_test_vect)
+        classes3 = np.argmax(preds3, axis=1)
+
+        #Majority vote
+        final_classes = []
+        for i in range(len(classes1)):
+            if classes1[i] == classes2[i] and classes2[i] == classes3[i]:
+                final_classes.append(classes1[i])
+            elif classes1[i] == classes2[i] and classes2[i] != classes3[i]:
+                final_classes.append(classes1[i])
+            elif classes1[i] == classes3[i] and classes1[i] != classes2[i]:
+                final_classes.append(classes1[i])
+            elif classes2[i] == classes3[i] and classes2[i] != classes1[i]:
+                final_classes.append(classes2[i])
+            else:
+                final_classes.append(0)
+
+        final_classes = np.array(final_classes)
+        print(classification_report(y_test_vect, final_classes))
 
 if __name__ == "__main__":
     import json
@@ -202,5 +367,6 @@ if __name__ == "__main__":
     # wandb.init(project="CNN-Magic", name = log_name, entity="qinjerem", config=params)
 
     experiment(params)
+    print("Done")
 
     # wandb.finish()
